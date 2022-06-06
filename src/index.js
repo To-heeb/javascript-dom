4115e0a2f3907ac8f72110f866eff8434dd3a6b2
Toheeb Oyekola <Toheeb.Olawale.TO23@gmail.com>
2022-06-06 23:47:40 +0100

    creating of element

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import '../assets/css/style.css';
 
 const app = document.getElementById('app');
-app.innerHTML = `
-<h1>JavaScript DOM!!</h1>
-`;
+const div = document.createElement('div');
+const text = document.createTextNode('DOM!')
+const comment = document.createComment('No comment')
+
+div.append(comment)
+div.append(text)
+app.append(div)
+console.log(app, div)
 
 
 
 // <html>
 console.log(document)
+console.dir(document)
 console.dir(document.documentElement)
 
 // <head>
@@ -17,6 +23,18 @@ console.dir(document.head)
 // <body>
 console.dir(document.body)
 
+// retrieve the contructor name from
+console.log(document.body.constructor.name)
+
+// looking at the prototype chain
+console.log(document.body instanceof HTMLBodyElement)
+console.log(document.body instanceof HTMLElement)
+console.log(document.body instanceof Element)
+console.log(document.body instanceof Node)
+console.log(document.body instanceof EventTarget)
+console.log(document.body instanceof Object)
+
+
 // Onload of DOM content
 /*document.addEventListener('DOMContentLoaded', () => {
   alert('DOMContentLoaded')
@@ -24,3 +42,28 @@ console.dir(document.body)
 
 // On load of page
 //window.addEventListener('load', () => { alert('Load') })
+
+/*
+  - NodeTypes
+  1: Element
+  2: Attribute
+  3: Text
+  4: CDATASection
+  5: EntityReference
+  6: Entity
+  7: ProcessingInstruction
+  8: Comment
+  9: Document
+  10: DocumentType
+  11: DocumentFragment
+  12: Notation
+*/
+
+
+console.log(document.body.nodeType)
+console.log(document.nodeType)
+
+// nodeName form any Node types
+console.log(document.body.nodeName)
+// tagName for any Element type
+console.log(document.body.tagName)
