723f59a1f3c736784ae45b87783e2aa970f6f554
Toheeb Oyekola <Toheeb.Olawale.TO23@gmail.com>
2022-06-07 17:17:11 +0100

    replacing dom element

--- src/index.js
+++ src/index.js
@@ -3,19 +3,27 @@ import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = `
 <h1>JavaScript DOM!!</h1>
-<ul>
-  <li>1</li>
-</ul>
+<div>
+  Replace me!
+</div>
 `;
 
-const ul = app.querySelector('ul');
-ul.insertAdjacentHTML('beforebegin', '<p>Before</p>');
-ul.insertAdjacentHTML('afterbegin', '<li>First</li>');
-ul.insertAdjacentHTML('beforeend', '<li>Last</li>');
-ul.insertAdjacentHTML('afterend', '<p>After</p>');
+const div = app.querySelector('div');
+const newDiv = document.createElement('div');
+newDiv.innerHTML = 'I have been replaced';
 
-console.log(app)
+// new way
+div.replaceWith(newDiv);
 
+// old way
+const anotherDiv = document.createElement('div');
+anotherDiv.innerHTML = 'I replace all';
+
+setTimeout(() => {
+  newDiv.parentNode.replaceChild(anotherDiv, newDiv);
+}, 2000)
+
+console.log(div)
 
 /*
   - NodeTypes
